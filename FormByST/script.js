2a476bb35a0cfc6a0c8a79aac98df03d501d68bf
Shashwattejaswi <shashwatteju06@gmail.com>
2022-06-02 16:41:48 +0530

    arrange numbers of historyLog's inputEle

--- FormByST/script.js
+++ FormByST/script.js
@@ -92,14 +92,46 @@ function elelistcloser(tager){    /*tager is elementList id's element*/
             }
         },200);
 }
+function deleteinput(){
+    delHistoryEle(this.parentElement);
+}
+const updateNumEle={
+    textbox:0,radio:0,range:0,
+    updigit:function(a){
+        switch (a) {
+        case 'textbox':
+            this.textbox+=1;
+             return this.textbox; 
+        case 'radio': 
+        this.radio+=1;
+             return this.radio; 
+         case 'range': 
+         this.range+=1;
+              return this.range;
+        default:
+            break;
+        }
+    } 
+};
+const inputObj=[];
 
-function addHistoryEle(eleDetail={input:'textbox',max:20,min:10,label:'firstname'}){
+function addHistoryEle(eleDetail={input:'radio',max:20,min:10,label:'firstname'}){
+    let count=0;
     let historydiv=document.getElementById('historyPanel');
     let divele=document.createElement('div');
     deleteSpan=document.createElement('span');
+    deleteSpan.addEventListener('click',deleteinput);
     deleteSpan.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z"/></svg>';
     divele.appendChild(deleteSpan);
-    labels=document.createTextNode(eleDetail.input+1);
-    divele.appendChild(labels);
+    count=updateNumEle.updigit(eleDetail.input);
+    labels=document.createTextNode(eleDetail.input+'-'+count);
+    paragrph=document.createElement('p');
+    paragrph.appendChild(labels);
+    divele.appendChild(paragrph);
     historydiv.appendChild(divele);
+    inputObj.push(eleDetail);
+}
+function delHistoryEle(parentDiv){
+    alert(parentDiv.children[1].innerHTML);
+    parentDiv.remove();
 }
