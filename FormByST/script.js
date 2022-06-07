828a8f58c6134721414de5476d14bf24b57855e4
shashwattejaswi <shashwatteju06@gmail.com>
2022-06-07 20:07:07 +0530

    email displayed

--- FormByST/script.js
+++ FormByST/script.js
@@ -6,27 +6,32 @@ if (/^[a-zA-Z ]+$/.test(t.value.charAt(t.value.length-1))||t.value=="") {
      t.value=t.value.slice(0,t.value.length-1);
 }   
 }
+
+
 function changeTextToDate(a){
     a.type="date";
 }
 
+
 function advanceOpener(addvance){
     let detailDiv= addvance.previousElementSibling;
-        detailDiv.style.cssText="height:8%;top:2%;";
-        detailDiv.children[detailDiv.children.length-1].style.cssText="display:block;filter:opacity(1);";
-        addvance.children[addvance.children.length-1].style.cssText="display:none;filter:opacity(0);";
-        addvance.style.cssText="height:71.5%;border-color:transparent;background-color:transparent;cursor:default;";
-        for(i=0;i<detailDiv.children.length-1;i++){
-            detailDiv.children[i].style="filter:opacity(0)";
-            if(i>=0&&i<addvance.children.length-1){
-                addvance.children[i].style="display:inline-block;";
-            }
+    detailDiv.style.cssText="height:8%;top:2%;";
+    detailDiv.children[detailDiv.children.length-1].style.cssText="display:block;filter:opacity(1);";
+    addvance.children[addvance.children.length-1].style.cssText="display:none;filter:opacity(0);";
+    addvance.style.cssText="height:71.5%;border-color:transparent;background-color:transparent;cursor:default;";
+    for(i=0;i<detailDiv.children.length-1;i++){
+        detailDiv.children[i].style="filter:opacity(0)";
+        if(i>=0&&i<addvance.children.length-1){
+            addvance.children[i].style="display:inline-block;";
         }
-        setTimeout(function(){
-            for(i=0;i<10;i++)
-                detailDiv.childNodes[1].style="display:none";
-        },400);	
+     }
+    setTimeout(function(){
+        for(i=0;i<10;i++)
+            detailDiv.childNodes[1].style="display:none";
+    },400);	
 }
+
+
 function detailOpener(detailDiv){
     let add=detailDiv.nextElementSibling;
         detailDiv.style.cssText="height:64%;top:4%;";
@@ -52,7 +57,13 @@ function detailOpener(detailDiv){
                 },330);
                 
 }
+
+
  function forPopupOpen() {addElementPopup(this.innerHTML);}
+function outputForEmail(){ 
+    let objEmail={input:'email'};
+    addHistoryEle(objEmail);
+}
 
 function elelistopener(mainDiv){
     let ele=mainDiv.nextElementSibling;
@@ -66,7 +77,9 @@ function elelistopener(mainDiv){
          }
          for(let l=0;l<ele.children.length;l++){
              if(l!=4)
-         ele.children[l].addEventListener('click',forPopupOpen);
+                 ele.children[l].addEventListener('click',forPopupOpen);
+            else if(l==4)
+                 ele.children[l].addEventListener('click',outputForEmail); 
          }
          for(let j=0;j<=ele.children.length;j++){
             setTimeout(function(){
@@ -78,23 +91,27 @@ function elelistopener(mainDiv){
         elelistcloser(ele);
     }
 }
+
+
 function elelistcloser(tager){    /*tager is elementList id's element*/
     tager.style.cssText="bottom:5vw;filter:opacity(0);";
     for(let i=0;i<tager.children.length;i++){
     tager.children[i].style.filter="opacity(0)";
     tager.children[i].style.margin="0vw";
 }
-        setTimeout(function(){
-            tager.style.cssText="height:0%;";
-            for(let j=0;j<tager.children.length;j++){
-                tager.children[j].style.margin=".4vw";
-                tager.children[j].style.display="none";
-            }
-        },200);
+     setTimeout(function(){
+         tager.style.cssText="height:0%;";
+         for(let j=0;j<tager.children.length;j++){
+             tager.children[j].style.margin=".4vw";
+             tager.children[j].style.display="none";
+         }
+     },200);
 }
+
 function deleteinput(){
     delHistoryEle(this.parentElement);
 }
+
 const updateNumEle={
     textbox:0,radio:0,range:0,
     updigit:function(a){
@@ -113,6 +130,7 @@ const updateNumEle={
         }
     } 
 };
+
 const inputObj=[];
 
 function addHistoryEle(eleDetail){
@@ -122,6 +140,7 @@ function addHistoryEle(eleDetail){
     deleteSpan=document.createElement('span');
     deleteSpan.addEventListener('click',deleteinput);
     deleteSpan.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z"/></svg>';
+    
     divele.appendChild(deleteSpan);
     count=updateNumEle.updigit(eleDetail.input);
     labels=document.createTextNode(eleDetail.input+'-'+count);
@@ -129,6 +148,7 @@ function addHistoryEle(eleDetail){
     paragrph=document.createElement('p');
     paragrph.appendChild(labels);
     divele.appendChild(paragrph);
+    
     historydiv.appendChild(divele);
     pos=historydiv.scrollHeight-historydiv.clientHeight;
     historydiv.scrollTop=pos;
@@ -136,8 +156,8 @@ function addHistoryEle(eleDetail){
     let displayedEle=showInOutput(eleDetail);
     eleDetail.outputedEle=displayedEle;
 }
+
 function delHistoryEle(parentDiv){ /* div in which "inputname" and delete icon situtated */
-    
     let names=parentDiv.children[1].innerHTML;
         for(let i=inputObj.length;i>0;i--){
             if(inputObj[i-1].id==names.slice(0,names.length))
@@ -155,7 +175,7 @@ function showInOutput(allDetail){
     dynEle=document.createElement('input');
     dynEle.setAttribute('type',allDetail.input);
     dynEle.setAttribute('id',allDetail.id);
-    dynEle.setAttribute('placeholder',allDetail.main_label);
+    dynEle.setAttribute('placeholder',allDetail.id);
     outputDiv.appendChild(dynEle);
     return dynEle; /* return the current created ele so that we can add this ele to their detailed object */
 }
